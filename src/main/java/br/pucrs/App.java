81fe975f6f45bf72507c69856e94ef766fc20e6c
Vinícius Dias <108445811+vinipdiass@users.noreply.github.com>
2024-08-19 20:38:06 -0300

    teste 1

--- src/main/java/br/pucrs/App.java
+++ src/main/java/br/pucrs/App.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 package br.pucrs;
 
 /**
@@ -9,5 +10,45 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+    
+    public ArrayList<Integer> mergeSort(ArrayList<Integer> lista){
+        int tamanho = lista.size();
+        if (tamanho == 1) return lista.get(0);
+
+
+
+        ArrayList<Integer> a = mergeSort(lista.subList(0, tamanho/2));
+        ArrayList<Integer> b = mergeSort(lista.subList(tamanho/2 + 1, tamanho-1));
+        
+        ArrayList<Integer> l = new ArrayList<Integer>();
+
+
+
+
+        while (!a.isEmpty() && !b.isEmpty()){
+            if (a.getFirst() < b.getFirst()) {
+                Integer e = a.remove(0);
+                l.add(e);
+            } else {
+                Integer e = b.remove(0);
+                l.add(e);
+            }
+        }
+
+        if (a.isEmpty()) {
+            while (!b.isEmpty()) {
+                Integer e = b.removeFirst();
+                l.add(e);
+            }
+        } else if (b.isEmpty()) {
+            while (!b.isEmpty()) {
+                Integer e = b.removeFirst();
+                l.add(e);
+            }
+        }
+
+
+
     }
 }
